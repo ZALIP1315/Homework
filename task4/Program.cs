d90c10bec8a2bfc6d58a34cb7c562b7aa0f3dce5
Kirill <ksfomin1315@gmail.com>
2022-09-20 20:54:09 +0300

    r

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// task4
+int numberN = 32554;
+int numberX = numberN /2;
+int numberY = numberX *2;
+int numberA = 2;
+
+if (number == numberN)
+{
+numberN = numberN;  
+}
+else 
+{
+ numberN = numberN -1;
+}
+
+while (numberA <= numberN)
+{
+Console.WriteLine(numberA);
+numberA = numberA+2;  
+}
+ 
+
+ 
+
