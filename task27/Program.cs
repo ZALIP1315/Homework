c3c4c13e7aeb3c5ca3923dad5c6ae55972245bd5
Kirill <ksfomin1315@gmail.com>
2022-09-20 21:10:33 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// task 27
+Console.WriteLine("Введите число ");
+int number= Convert.ToInt32(Console.ReadLine());
+number=Math.Abs(number);
+int count = Convert.ToString(number).Length;
+int res = 0;
+int n2 =0;
+for (int i = 0;i< count;i++)
+{
+    n2=number-number%10;
+    res=res+(number-n2);
+    number=number/10;
+}
+Console.WriteLine($"{res}");
