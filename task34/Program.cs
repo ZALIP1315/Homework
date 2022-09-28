ce43948bdcdea2dcfa74eaa456e35782da64a763
Kirill <ksfomin1315@gmail.com>
2022-09-28 23:40:03 +0300

    TASK34

---
+++ task34/Program.cs
@@ -0,0 +1,23 @@
+﻿// task34
+int[] GetArray(int size,int leftRange,int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0;i<size;i++)
+    {
+        arr[i]=rand.Next(leftRange,rightRange + 1);
+    }
+    return arr;
+}
+
+int[ ] array = GetArray(4,100,999);
+Console.WriteLine(string.Join(", ",array));
+int count = 0;
+for(int i=0;i<array.Length;i++)
+{
+    if (array [i] == (array [i]/2)*2)
+      {
+        count++;
+    }
+}
+Console.WriteLine($"количество чётных чисел в массиве = {count}");
